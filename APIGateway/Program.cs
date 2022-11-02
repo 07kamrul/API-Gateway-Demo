1d7ab8e3e4151fcaa3dbe85cc1815fc50007ec4e
Kamrul Hasan <kamrul@asabd.org>
2022-11-02 12:02:04 +0600

    Program file configuration with .net 6

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,79 +1,46 @@
-/*using Microsoft.AspNetCore;
+using Microsoft.AspNetCore;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(
+    p => p.AddPolicy("corsapp", builder =>
+    {
+        builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    })
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var ocelotBuilder = WebHost.CreateDefaultBuilder(args);
+builder.Services.AddOcelot();
 
-*//*ocelotBuilder.ConfigureServices(s =>
-        s.AddSingleton(ocelotBuilder))
-        .ConfigureAppConfiguration(c => 
-        c.AddJsonFile("ocelot.json"));*//*
-
-ocelotBuilder.UseUrls("http://*:9000")
-             .ConfigureAppConfiguration((hostingContext, config) =>
-             {
-                 config
-                     .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                     .AddJsonFile("ocelot.json")
-                     .AddEnvironmentVariables();
-             })
-            .ConfigureServices(services =>
-            {
-                services.AddOcelot()
-                    .AddConsul();
-            })
-            .Configure(app =>
-            {
-                app.UseMvc()
-                .UseSwagger()
-                .UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/a/swagger.json", "Catalog");
-                    c.SwaggerEndpoint("/b/swagger.json", "Product");
-                });
-                app.UseOcelot().Wait();
-            });
+builder.Configuration.AddJsonFile("ocelot.json");
 
 var app = builder.Build();
 
 app.UseAuthorization();
 
+app.UseOcelot();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
+
+
 app.MapControllers();
 
-app.Run();
-
-*/
-
-
-
-namespace APIGateway
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            })
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config.AddJsonFile("ocelot.json");
-            });
-    }
+    app.Run();
 }
+else
+{
+    app.Run("http://0.0.0.0:5000");
+}
+
+
